ce92c7975afa13cf94282821229066599c4030c6
Flibouche <kevin.pfiffer2@gmail.com>
2025-02-12 15:51:38 +0100

    Ajoutez des fichiers projet.

---
+++ ASPDotNet_Core_MVC_Tuto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPDotNet_Core_MVC_Tuto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
