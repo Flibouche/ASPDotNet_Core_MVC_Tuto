e99534dc2beeda8ee516e0107f7986c8cc40af2e
Flibouche <kevin.pfiffer2@gmail.com>
2025-02-12 16:12:33 +0100

    Create new controller, new item and first view to see the MVC in action.

---
+++ ASPDotNet_Core_MVC_Tuto/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using ASPDotNet_Core_MVC_Tuto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNet_Core_MVC_Tuto.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "keyboard" };
+            return View(item);
+        }
+    }
+}
---
+++ ASPDotNet_Core_MVC_Tuto/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace ASPDotNet_Core_MVC_Tuto.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
